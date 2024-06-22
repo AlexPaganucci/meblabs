2aca78b3d4586d673c1ef9b130181e97f3b7150f
AlexPaganucci <114147785+AlexPaganucci@users.noreply.github.com>
2024-06-22 08:36:26 +0200

    modificato modello spesa

--- Api/models/expenseModel.js
+++ Api/models/expenseModel.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Tipo ObjectId per riferire un utente nel database
+    ref: 'User', // Riferimento al modello User
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
   amount: {
     type: Number,
     required: true
